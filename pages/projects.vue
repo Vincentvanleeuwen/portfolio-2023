defc676d2d6f56f342d37448203a4b41feb07b3a
Vincent van Leeuwen <shoockz@hotmail.com>
2023-05-27 10:43:52 +0200

    Added hover effect

--- pages/projects.vue
+++ pages/projects.vue
@@ -9,14 +9,20 @@
             :key="project.id"
             class="Triangle"
             @click="$router.push(`project/${project.id}`)"
+            :aria-label="`Open the ${project.id}} project`"
           >
-            <p style="color: white">{{ project.title }}</p>
-            <SvgRinus v-if="project.title === 'Rinus'" />
-            <SvgMomkai v-else-if="project.title === 'Momkai'" />
-            <SvgNFPConnects v-else-if="project.title === 'NFP'" />
-            <SvgDEN v-else-if="project.title === 'DEN'" />
-            <SvgPraPla v-else-if="project.title === 'PraPla'" />
-            <SvgCombinify v-else-if="project.title === 'Combinify'" />
+            <div class="Triangle-inner">
+              <!-- <p style="color: white">{{ project.title }}</p> -->
+              <SvgRinus v-if="project.title === 'Rinus'" />
+              <SvgMomkai v-else-if="project.title === 'Momkai'" />
+              <SvgNFPConnects v-else-if="project.title === 'NFP'" />
+              <SvgDEN v-else-if="project.title === 'DEN'" />
+              <SvgPraPla v-else-if="project.title === 'PraPla'" />
+              <SvgCombinify v-else-if="project.title === 'Combinify'" />
+            </div>
+            <span class="Triangle-button">
+              Read about {{ project.title }}
+            </span>
           </div>
         </section>
       </template>
@@ -64,46 +70,46 @@ const projects = projectStore.projects;
     transform-origin: top center;
   }
 }
-// .Triangle {
-//   position: relative;
-// }
 .Triangle {
   position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
   width: $triangle-width;
   height: $triangle-height;
-  background: black;
+  overflow: hidden;
   clip-path: polygon(0 0, 100% 50%, 0 100%);
   cursor: pointer;
-
-  svg {
-    position: absolute;
-    width: 120px;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
 
   &:nth-child(even) {
     clip-path: polygon(100% 0, 0 50%, 100% 100%);
   }
-
   &:first-child {
     grid-row: 1 / 2;
     grid-column: 1;
-    background: #00aadb;
+    background: url("~/assets/images/Rinus.png") center center no-repeat;
+    background-size: contain;
     svg {
       transform: translate(-75%, -50%);
+    }
+    .Triangle-inner {
+      background: #00aadb;
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      // TODO: To change this color.
+      background-image: linear-gradient(to bottom right, #002f4b, #dc4225);
+      opacity: 0.6;
     }
   }
   &:nth-child(2) {
     margin-top: -120px;
     grid-row: 2 / 3;
     grid-column: 1;
-    background: #000000;
+    background: url("~/assets/images/Momkai.png") center center no-repeat;
+    background-size: contain;
 
     @include breakpoint(medium) {
       margin-top: -175px;
@@ -114,12 +120,27 @@ const projects = projectStore.projects;
       width: 80px;
       transform: translate(-00%, -50%);
     }
+    .Triangle-inner {
+      background: #000000;
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      // TODO: To change this color.
+      background-image: linear-gradient(to bottom right, #002f4b, #dc4225);
+      opacity: 0.6;
+    }
   }
   &:nth-child(3) {
     margin-top: 60px;
     grid-row: 2 / 3;
     grid-column: 1;
-    background: #480e30;
+    background: url("~/assets/images/NFPConnects.png") center center no-repeat;
+    background-size: contain;
 
     @include breakpoint(medium) {
       margin-top: -175px;
@@ -130,12 +151,27 @@ const projects = projectStore.projects;
       width: 150px;
       transform: translate(-60%, -50%);
     }
+    .Triangle-inner {
+      background: #480e30;
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      // TODO: To change this color.
+      background-image: linear-gradient(to bottom right, #002f4b, #dc4225);
+      opacity: 0.6;
+    }
   }
   &:nth-child(4) {
     margin-top: -120px;
     grid-row: 3;
     grid-column: 1;
-    background: #ffe3ed;
+    background: url("~/assets/images/DEN.png") center center no-repeat;
+    background-size: contain;
 
     @include breakpoint(medium) {
       margin-top: -175px;
@@ -146,12 +182,28 @@ const projects = projectStore.projects;
     svg {
       transform: translate(-30%, -50%);
     }
+    .Triangle-inner {
+      background: #ffe3ed;
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      // TODO: To change this color.
+      background-image: linear-gradient(to bottom right, #002f4b, #dc4225);
+      opacity: 0.6;
+    }
   }
   &:nth-child(5) {
     margin-top: -120px;
     grid-row: 4;
     grid-column: 1;
-    background: #bfebaf;
+    background: url("~/assets/images/Prapla.png") center center no-repeat;
+    background-size: contain;
+
     @include breakpoint(medium) {
       margin: 0;
       grid-row: 3 / 4;
@@ -161,12 +213,28 @@ const projects = projectStore.projects;
       width: 100px;
       transform: translate(-85%, -50%);
     }
+    .Triangle-inner {
+      background: #bfebaf;
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      // TODO: To change this color.
+      background-image: linear-gradient(to bottom right, #002f4b, #dc4225);
+      opacity: 0.6;
+    }
   }
   &:last-child {
     margin-top: 60px;
     grid-row: 4;
     grid-column: 1;
-    background: #101010;
+    background: url("~/assets/images/Combinify.png") center center no-repeat;
+    background-size: contain;
+
     @include breakpoint(medium) {
       margin: 0;
       grid-row: 3 / 4;
@@ -180,12 +248,63 @@ const projects = projectStore.projects;
       top: 50%;
       transform: translate(-38%, -50%);
     }
+    .Triangle-inner {
+      background: #101010;
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      // TODO: To change this color.
+      background-image: linear-gradient(to bottom right, #002f4b, #dc4225);
+      opacity: 0.6;
+    }
   }
 }
 
-button {
-  max-height: 30px;
-  margin-bottom: 0.5rem;
-  max-width: 120px;
+.Triangle-inner {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+  transition: all 0.5s cubic-bezier(1, -0.03, 0.83, 0.67);
+  background: rgb(224, 229, 234);
+  z-index: 2;
+  // clip-path: polygon(0 0, 100% 50%, 0 100%);
+
+  svg {
+    position: absolute;
+    width: 120px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+
+.Triangle:nth-child(odd):hover .Triangle-inner {
+  transform: translateX(100%);
+}
+.Triangle:nth-child(even):hover .Triangle-inner {
+  transform: translateX(-100%);
+}
+.Triangle-button {
+  position: absolute;
+  white-space: nowrap;
+  font-family: Raleway;
+  font-weight: 800;
+  color: $c-white;
+  z-index: 0;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-decoration: underline;
+  text-underline-offset: 2px;
+  cursor: pointer;
 }
 </style>
