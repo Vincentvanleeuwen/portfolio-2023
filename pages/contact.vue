bdc1cb78c6d7346c47f59c1c87727c79c97ba868
Vincent van Leeuwen <shoockz@hotmail.com>
2023-07-21 13:19:30 +0200

    added a form to contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,19 @@
 <template>
   <section>
-    <suspense>
-      <template #default> </template>
-
-      <template #fallback> Loading... </template>
-    </suspense>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </section>
 </template>
 
